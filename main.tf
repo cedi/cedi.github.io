b8ca489379d70c7733d1da69505da015fea180a6
Cedric Kienzler <cekienzl@microsoft.com>
2020-01-22 00:59:03 +0000

    changes

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
 provider "azurerm" {
 }
 
-terraform {
-    backend "azurerm" {}
-}
-
 resource "azurerm_resource_group" "cedidev_rg" {
-  name = "${var.resource_group_name}"
-  location = "${var.location}"
+  name = "cedi_dev"
+  location = "northeurope"
 }
